13d2e725ca2a321ca167530caf0ec96e3cd80638
Hasan Maki <42813480+nekadcoding@users.noreply.github.com>
2024-11-13 22:34:41 +0700

    Refactor MapPanel; add button creation method, implement tile shuffling on click, and enhance tile color retrieval

--- src/main/java/com/coffeeisoxygen/view/MapPanel.java
+++ src/main/java/com/coffeeisoxygen/view/MapPanel.java
@@ -3,11 +3,14 @@ package com.coffeeisoxygen.view;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import com.coffeeisoxygen.model.tiles.Tile;
+import com.coffeeisoxygen.model.tiles.TileType;
 import com.coffeeisoxygen.viewmodel.BoardViewModel;
 
 public class MapPanel extends JPanel {
@@ -32,10 +35,8 @@ public class MapPanel extends JPanel {
 
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                JButton button = new JButton(i + "," + j);
-                button.setBackground(getTileColor(viewModel.getTile(i, j)));
+                JButton button = createButton(i, j);
                 buttons[i][j] = button;
-
                 gbc.gridx = i;
                 gbc.gridy = j;
                 add(button, gbc);
@@ -43,24 +44,54 @@ public class MapPanel extends JPanel {
         }
     }
 
+    private JButton createButton(int i, int j) {
+        JButton button = new JButton(i + "," + j);
+        button.setBackground(getTileColor(viewModel.getTile(i, j)));
+        button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                shuffleTileType(i, j);
+            }
+        });
+        return button;
+    }
+
     private Color getTileColor(Tile tile) {
         if (tile == null) {
             return Color.WHITE;
         }
-        return switch (tile.getTileType()) {
-            case STARTTILE -> Color.GREEN;
-            case FINISHTILE -> Color.RED;
-            case SAFETILE -> Color.BLUE;
-            case DANGERTILE -> Color.ORANGE;
-            case ROUTETILE -> Color.GRAY;
-        };
+        switch (tile.getTileType()) {
+            case STARTTILE: return Color.GREEN;
+            case FINISHTILE: return Color.RED;
+            case SAFETILE: return Color.BLUE;
+            case DANGERTILE: return Color.ORANGE;
+            case ROUTETILE: return Color.GRAY;
+            default: return Color.WHITE;
+        }
+    }
+
+    private void shuffleTileType(int x, int y) {
+        Tile tile = viewModel.getTile(x, y);
+        if (tile != null) {
+            // Acak TileType (Selain STARTTILE dan FINISHTILE)
+            TileType newTileType = randomizeTileType();
+            viewModel.setTile(x, y, newTileType);  // Update di viewModel
+            buttons[x][y].setBackground(getTileColor(viewModel.getTile(x, y))); // Update tampilan tombol
+        }
+    }
+
+    private TileType randomizeTileType() {
+        // Mengacak antara DANGERTILE, SAFETILE, dan ROUTETILE
+        TileType[] tileTypes = {TileType.DANGERTILE, TileType.SAFETILE, TileType.ROUTETILE};
+        int randomIndex = (int) (Math.random() * tileTypes.length);
+        return tileTypes[randomIndex];
     }
 
     public void refresh() {
         int width = viewModel.getBoardWidth();
         int height = viewModel.getBoardHeight();
 
-        // Reinitialize buttons if the board dimensions have changed
+        // Reinitialize buttons jika ukuran board berubah
         if (buttons.length != width || buttons[0].length != height) {
             removeAll();
             initializeButtons();
