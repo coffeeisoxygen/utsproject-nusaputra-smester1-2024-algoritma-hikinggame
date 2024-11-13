08ecf53c0a4c39a10c11d3137c654d8290d9eb8f
Hasan Maki <42813480+nekadcoding@users.noreply.github.com>
2024-11-13 15:07:23 +0700

    Implement BoardObserver in BoardViewModel to handle board changes and update UI

--- src/main/java/com/coffeeisoxygen/viewmodel/BoardViewModel.java
+++ src/main/java/com/coffeeisoxygen/viewmodel/BoardViewModel.java
@@ -1,13 +1,18 @@
 package com.coffeeisoxygen.viewmodel;
 
+import com.coffeeisoxygen.interfaces.BoardObserver;
 import com.coffeeisoxygen.model.tiles.Board;
 import com.coffeeisoxygen.model.tiles.Tile;
 
-public class BoardViewModel {
+public class BoardViewModel implements BoardObserver {
     private Board board;
 
     public BoardViewModel(Board board) {
         this.board = board;
+    }
+
+    public void initialize() {
+        this.board.addObserver(this);
     }
 
     public Tile[][] getTiles() {
@@ -26,7 +31,9 @@ public class BoardViewModel {
         return board.getTile(x, y);
     }
 
-    public Board getBoard() {
-        return board;
+    @Override
+    public void boardChanged() {
+        // Update the UI or perform other actions when the board changes
+        System.out.println("Board has changed!");
     }
 }
